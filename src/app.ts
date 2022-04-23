be640cfb07d6f667224fe371fc271b825795b09b
Paolo Dapul <paolodapultech@gmail.com>
2022-04-23 21:27:24 +0800

    Create an Express instance

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Welcome to Node Starter | Made with <3 by @paolodapul`);
+});
+
+app.listen(port, () => {
+  return console.log(
+    `Express is listening at http://localhost:${process.env.PORT}`
+  );
+});
