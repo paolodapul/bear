b3e752dd3edca213bc390265eb62963d1e9de48d
Paolo Dapul <paolodapultech@gmail.com>
2022-11-05 16:46:44 +0800

    Configure logging

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,46 @@
-import dotenv from 'dotenv'
-import express from 'express'
+import dotenv from "dotenv";
+import express from "express";
+import { format, transports } from "winston";
+import expressWinston from "express-winston";
+import log from "loglevel";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
-app.get('/', (req, res) => {
-  res.send(`Welcome to Node Starter | Made with <3 by @paolodapul`)
-})
+const app = express();
+const port = process.env.PORT || 3000;
+
+const logFormat = format.combine(
+  format.label({ label: "worker" }),
+  format.timestamp({
+    format: "HH-MM:ss YYYY-MM-DD",
+  }),
+  format.prettyPrint(),
+  format.colorize(),
+  format.align(),
+  format.printf((info) => {
+    const timeStamp = info.timestamp as string;
+    const label = info.label as string;
+    const message = info.message as string;
+    const level = info.level;
+
+    return `[${timeStamp}] [${label}]@[${level}]: ${message.trim()}\n`;
+  })
+);
+
+app.use(
+  expressWinston.logger({
+    transports: [new transports.Console()],
+    format: logFormat,
+    expressFormat: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send(
+    `Bear.js - opinionated Node.js starter | Made with ❤️ by Paolo Dapul`
+  );
+});
 
 app.listen(port, () => {
-  return console.log(
-    `Express is listening at http://localhost:${process.env.PORT}`,
-  )
-})
+  log.warn(`Express is listening at http://localhost:${process.env.PORT}\n`);
+});
