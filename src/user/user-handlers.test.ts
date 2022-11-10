efbf13ee35055dfc1db4d3177653bc8eedef049a
Paolo Dapul <paolodapultech@gmail.com>
2022-11-10 20:48:25 +0800

    Update test suite for user handlers

--- src/user/user-handlers.test.ts
+++ src/user/user-handlers.test.ts
@@ -2,23 +2,25 @@ import request from "supertest";
 import { app } from "../app";
 import { validateParams } from "./user-handlers";
 
-describe("user handler tests", () => {
-  it("Should return the query params as JSON", async () => {
-    const res = await request(app).get("/test?name=Bear");
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe(JSON.stringify({ name: "Bear" }));
-  });
+describe("User handler tests", () => {
+  describe("When /test URL is called", () => {
+    it("If query string parameters are correct, it should return the query params as JSON", async () => {
+      const res = await request(app).get("/test?name=Bear");
+      expect(res.statusCode).toBe(200);
+      expect(res.text).toBe(JSON.stringify({ name: "Bear" }));
+    });
 
-  it("Should return a 400 error if query params are incorrect", async () => {
-    const req = request(app);
-    const res = await req.get("/test?id=Bear");
-    expect(() => validateParams({ id: "Bear" })).toThrow();
-    expect(res.statusCode).toBe(400);
-    expect(res.text).toBe(
-      JSON.stringify({
-        error: "Bad request",
-        message: "You may have passed incorrect values on your query string.",
-      })
-    );
+    it("If query params are incorrect, it should return a 400 status error", async () => {
+      const req = request(app);
+      const res = await req.get("/test?id=Bear");
+      expect(() => validateParams({ id: "Bear" })).toThrow();
+      expect(res.statusCode).toBe(400);
+      expect(res.text).toBe(
+        JSON.stringify({
+          error: "Bad request",
+          message: "You may have passed incorrect values on your query string.",
+        })
+      );
+    });
   });
 });
