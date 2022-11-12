576d72d6766041973256c5c5d72201e9d3811660
Paolo Dapul <paolodapultech@gmail.com>
2022-11-12 18:40:13 +0800

    Update test to use Express instance and updated users route

--- src/user/user-handlers.test.ts
+++ src/user/user-handlers.test.ts
@@ -1,18 +1,18 @@
 import request from "supertest";
-import { app } from "../app";
+import { server } from "../app";
 import { validateParams } from "./user-handlers";
 
 describe("User handler tests", () => {
-  describe("When /test URL is called", () => {
+  describe("When /users URL is called", () => {
     it("If query string parameters are correct, it should return the query params as JSON", async () => {
-      const res = await request(app).get("/test?name=Bear");
+      const res = await request(server).get("/users?name=Bear");
       expect(res.statusCode).toBe(200);
       expect(res.text).toBe(JSON.stringify({ name: "Bear" }));
     });
 
     it("If query params are incorrect, it should return a 400 status error", async () => {
-      const req = request(app);
-      const res = await req.get("/test?id=Bear");
+      const req = request(server);
+      const res = await req.get("/users?id=Bear");
       expect(() => validateParams({ id: "Bear" })).toThrow();
       expect(res.statusCode).toBe(400);
       expect(res.text).toBe(
