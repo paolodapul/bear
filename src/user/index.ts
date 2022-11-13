71c0a7a371687003b2511392d7131c202aa86763
Paolo Dapul <paolodapultech@gmail.com>
2022-11-13 16:54:48 +0800

    Implement async route handlers

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 import * as userHandlers from "./user-handlers";
 
 const usersRouter = express.Router();
 
-usersRouter.get("/users", userHandlers.getUsersHandler);
+usersRouter.get("/users", asyncHandler(userHandlers.getUsersHandler));
 
 export { usersRouter };
